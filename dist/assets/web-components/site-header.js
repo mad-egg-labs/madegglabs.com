bf40af68b003370cb0a5d06243d91c44018c6045
Andrew Hedges <andrew@hedges.name>
2026-07-23 11:24:00 -0700

    Update header Web Component to support breadcrumbs

--- dist/assets/web-components/site-header.js
+++ dist/assets/web-components/site-header.js
@@ -1,14 +1,38 @@
 class SiteHeader extends HTMLElement {
   connectedCallback() {
-    const section = location.pathname.split('/')[1];
+    const buildBreadcrumb = () => {
+      // grab reference to element where we'll insert our HTML: <main><article>
+      const article = document.querySelector('main > article');
+
+      // grab reference to the page's title: <main><article><header><h2>
+      const title = article.querySelector('header > h2').innerText;
+
+      let lis = '';
+      const html = `
+        <nav aria-label="Breadcrumb">
+          <ol class="breadcrumbs">
+            <li><a href="/">Home</a></li>
+            <li><a href="/articles/">Articles</a></li>
+            <li><a href="/articles/${slug}/" aria-current="page">${title}</a></li>
+          </ol>
+        </nav>
+      `;
+
+      article.insertAdjacentHTML('afterbegin', html);
+    };
+
+    // parse pathname
+    const parts   = location.pathname.split('/');
+    const section = parts[1];
+    const slug    = parts.length > 2 ? parts[2] : null;
 
     // Usage: active.about, active.articles, etc.
     const active = {
       [section]: true
     };
 
+    const logo = 'apps' === section ? 'hat-egg.png' : 'mad-egg.png';
     const h1 = section ? '<h1><a href="/">Mad Egg Labs</a></h1>' : '<h1>Mad Egg Labs</h1>';
-
     const nav = section ? '' : `
       <nav>
         <p>
@@ -19,7 +43,10 @@ class SiteHeader extends HTMLElement {
       </nav>
     `;
 
-    const logo = 'apps' === section ? 'hat-egg.png' : 'mad-egg.png';
+    // no breadcrumb on the /articles landing page
+    if ('articles' === section && slug) {
+      document.addEventListener('DOMContentLoaded', buildBreadcrumb);
+    }
 
     this.innerHTML = `
       <header>
