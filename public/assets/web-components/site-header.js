d23dc93cd1f6edf91788fcca02830631abd9a6ec
Andrew Hedges <andrew@hedges.name>
2026-08-21 14:52:16 -0700

    Change to status

--- public/assets/web-components/site-header.js
+++ public/assets/web-components/site-header.js
@@ -27,7 +27,7 @@ class SiteHeader extends HTMLElement {
     const slug    = parts.length > 2 ? parts[2] : null;
 
     // are we on a 404 page?
-    const is404 = this.dataset.pageType === '404' || section.includes('404');
+    const is404 = this.dataset.status === '404' || section.includes('404');
 
     // Usage: active.about, active.articles, etc.
     const active = {
